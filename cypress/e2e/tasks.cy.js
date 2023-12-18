63bcd4828ebfc8481d2290b68a52938d2ed72b93
Mattheus Luiz <matth.luiz10@gmail.com>
2023-12-18 16:41:57 -0300

    Adionando o faker para massa teste

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,11 +1,13 @@
 /// <reference types="cypress"/>
 
+import {faker} from '@faker-js/faker'
 
 describe('tarefas', () => {
     it('Deve cadastrar uma nova tarefa', () => {
         cy.visit('http://localhost:8080')
 
         cy.get('input[placeholder="Add a new Task"]')
-            .type('Ler um livro de node.js')
+            .type(faker.music.songName())
+        cy.contains('button', 'Create').click()
     });
 });
