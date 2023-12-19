416bdad9ca69fc7556eb16faac0c6d09c3cc5259
Mattheus Luiz <matth.luiz10@gmail.com>
2023-12-19 09:07:15 -0300

    Adiconando contexto switch de cadastro

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,37 +1,39 @@
 /// <reference types="cypress"/>
 
 
-describe('tarefas', () => {
-    it('Deve cadastrar uma nova tarefa', () => {
-        const taskName = 'Ler um livro de Node.JS'
+describe('Tarefas', () => {
 
-
-        cy.removeTaskByName(taskName)
-        cy.createTask(taskName)
-
-        cy.contains('main div p', 'Ler um livro de Node.JS')
-    })
-
-
-    it('Não deve permitir tarefa duplicada', () => {
-        const task = {
-            name: 'Estudar JavaScript',
-            is_done: false
-        }
-
-        cy.removeTaskByName(task.name)
-        cy.postTask(task)
-        cy.createTask(task.name)
-
-        cy.get('.swal2-html-container')
-            .should('be.visible')
-            .should('have.text', 'Task already exists!')
+    context('cadastro', () => {
+        it('Deve cadastrar uma nova tarefa', () => {
+            const taskName = 'Ler um livro de Node.JS'
+    
+    
+            cy.removeTaskByName(taskName)
+            cy.createTask(taskName)
+    
+            cy.contains('main div p', 'Ler um livro de Node.JS')
+        })
+    
+    
+        it('Não deve permitir tarefa duplicada', () => {
+            const task = {
+                name: 'Estudar JavaScript',
+                is_done: false
+            }
+    
+            cy.removeTaskByName(task.name)
+            cy.postTask(task)
+            cy.createTask(task.name)
+    
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+        });
+    
+    
+        it('Campo obrigatorio', () => {
+            cy.createTask()
+            cy.isRequired('This is a required field')
+        })
     });
-
-
-    it('Campo obrigatorio', () => {
-        cy.createTask()
-        cy.isRequired('This is a required field')
-    })
-
 });
